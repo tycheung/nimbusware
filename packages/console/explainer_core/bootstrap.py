22a1f23578722b3a17f07cb2a6bc332e44fa2302
Tung Cheung <tungycheung@gmail.com>
2026-07-04 23:24:52 -0400

    Remove unused import from explainer bootstrap module.

--- packages/console/explainer_core/bootstrap.py
+++ packages/console/explainer_core/bootstrap.py
@@ -3,10 +3,7 @@ from __future__ import annotations
 import importlib
 
 from console.explainer_core.generic_workflow_explainer import install_explainer_metrics
-from console.explainer_core.workflow_explainer_registry import (
-    WORKFLOW_EXPLAINER_SPECS,
-    install_package_workflow_explainer_exports,
-)
+from console.explainer_core.workflow_explainer_registry import WORKFLOW_EXPLAINER_SPECS
 
 
 def bootstrap_standard_explainer(slug: str, namespace: dict[str, object]) -> None:
